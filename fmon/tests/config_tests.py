db3a5262bb63a536e243ccaf843523d6b3705c89
K. C. Juntunen <juntunen.kc@gmail.com>
2016-06-07 21:46:28 -0400

    updated tests

--- fmon/tests/config_tests.py
+++ fmon/tests/config_tests.py
@@ -1,11 +1,23 @@
 from unittest import TestCase
-#import fmon_config
-#from mongo_connection import MongoConnection
+from fmon.mongoconnection import MongoConnection
+from fmon.fmonconfig import FMonConfiguration
 
 class TestCreate(TestCase):
+    def setUp(self):
+        self.fmc = FMonConfiguration(
+            MongoConnection('localhost', 27017, '', ''))
+        
     def test_serial_port(self):        
-        # mc = MongoConnection('localhost', 27017, '', '')
-        # fmc = fmon_config.FMonConfiguration(mc)
-        # self.assertEquals(fmc.serial_port, '/dev/ttyAMA0')
-        pass
-    
+        self.assertEqual(self.fmc.port, '/dev/ttyACM0')
+        self.assertEqual(self.fmc.baudrate, 115200)
+
+    def test_sensors(self):
+        sensors = self.fmc.sensors
+        self.assertEqual(sensors[0]['sensor'], 'light')
+        self.assertEqual(sensors[0]['type'], 'timeseries')
+
+        self.assertEqual(sensors[1]['sensor'], 'hPa')
+        self.assertEqual(sensors[1]['type'], 'timeseries')
+
+        self.assertEqual(sensors[2]['sensor'], 'tempF')
+        self.assertEqual(sensors[2]['type'], 'timeseries')
--- fmon/tests/ip_tests.py
+++ fmon/tests/ip_tests.py
@@ -1,12 +1,11 @@
 from unittest import TestCase
-from fmon import ip
+from fmon.ip import *
 
 class TestCreate(TestCase):
     def test_get_ip_address(self):
-        a = ip.get_ip_address('lo')
+        a = get_ip_address('lo')
         self.assertEqual(a, '127.0.0.1')
 
     def test_all_interfaces(self):
-        a = ip.all_interfaces()
-        print(a)
-        self.assertTrue('lo' in a)
+        a = all_interfaces()
+        self.assertTrue('lo' in a)        
