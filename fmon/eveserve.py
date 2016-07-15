e9da584cb8deefd608e2c5da1707e371d0b5c00b
K. C. Juntunen <juntunen.kc@gmail.com>
2016-07-15 13:27:24 -0400

    ugh

--- fmon/eveserve.py
+++ fmon/eveserve.py
@@ -29,23 +29,7 @@ def lastreading(sensor):
         xmlerr = xml_wrap('_error', code + message)
         status = xml_wrap('_status', 'ERR')
         output =  xml_wrap('resource', xmlerr + status)
-        now = datetime.now()
-        print(now)
-        exp = datetime(now.year, now.month, now.day,
-                       now.hour, now.minute, now.second + 20)
-        print(exp)
-        header = {
-            'Content-Type': 'application/xml',
-            'Content-Length': len(output),
-            'Cache-Control': 'max-age=20',
-            'Expires': exp,
-            'Server': 'Eve/0.6.4 Werkzeug/0.11.3 Python/' + str(
-                sys.version.split()[0]),
-            'Date': now}
-        h = ''
-        for k in header:
-            h += '{}: {}\n'.format(k, header[k])
-        return '{}\n\n{}'.format(h, output)
+        return output
 
 def xml_wrap(tag, text):
     return '<' + tag + '>' + text + '</' + tag + '>'
