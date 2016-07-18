c93c758a597878e7b93ddc4fc623c3d121b178a7
K. C. Juntunen <juntunen.kc@gmail.com>
2016-07-18 13:23:40 -0400

    maybe it'll help to create the client here

--- fmon/thingspeak.py
+++ fmon/thingspeak.py
@@ -17,10 +17,10 @@ class ThingspeakInterface():
                                                  27017, '', '',
                                                  'arduinolog')
             self.conf = fmonconfig.FMonConfiguration(mc)
-            self.client = query.ArduinoLog()
             self.s = sched.scheduler(time.time, time.sleep)
             self._prev_dict = {}
             self.count = 0
+            self.oldcount = 0
 
     def tweet(self, message):
         if self.api_key == '':
@@ -66,17 +66,19 @@ class ThingspeakInterface():
         self.count += 1
         # In [7]: for x in al.ts_sensors:
         #    ...:     print(x, al.last_value(x))
-        dd = {k: self.client.last_value(k) for k in self.client.ts_sensors}
+        client = query.ArduinoLog()
+        dd = {k: client.last_value(k) for k in client.ts_sensors}
         if dd == self._prev_dict:
             print('{:4d} ({}): old dict => {}'.format(self.count, datetime.now(), dd))
-            self.client._database = None
-            self.client._tsdata = None
             self.s.enter(self.timespec, 1, self.send_data, ())
+            self.oldcount += 1
+            if self.oldcount > 20:
+                pass #exit(0)
             return False
 
         print('{:4d} ({}): new dict => {}'.format(self.count, datetime.now(), dd))
         self._prev_dict = dd
-
+        oldcount = 0
         try:
             params = self.create_url(dd)
             
