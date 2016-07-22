fb09493e71246c5a0bd8157d288cc58972e12bce
K. C. Juntunen <juntunen.kc@gmail.com>
2016-07-22 10:47:48 -0400

    let's make EveServer a class

--- fmon/eveserve.py
+++ fmon/eveserve.py
@@ -2,57 +2,67 @@
 
 import sys
 import logging
+from pymongo import MongoClient
 from fmon import query
 from eve import Eve
 from multiprocessing import Process
 from datetime import datetime
 from flask import request
 
-al = query.ArduinoLog()
-app = Eve()
-_proc = None
 
-@app.route('/sensors')
-def sensor_list():
-    return '\n'.join(al.all_sensors)
+class EveServer():
+    def __init__(self,
+                 mongoclient=MongoClient(host='localhost', port=27017),
+                 name='arduinolog'):
+        self.al = query.ArduinoLog(db=mongoclient, name=name)
+        self.app = Eve(settings='../settings.py')
 
-@app.route('/sensorcount')
-def sensor_count():
-    return str(len([x for x in al.all_sensors]))
+        # def decorator(f):
+        #     endpoint = options.pop('endpoint', None)
+        #     self.add_url_rule(rule, endpoint, f, **options)
+        #     return f
 
-@app.route('/lastreading/<sensor>')
-def lastreading(sensor):
-    if sensor in al.ts_sensors:
-        return str(al.last_value(sensor))
-    else:
-        msg = 'Unacceptible sensors. Please try: ' + ', '.join(al.ts_sensors)
-        code = xml_wrap('code', '406')
-        message = xml_wrap('message', msg)
-        xmlerr = xml_wrap('_error', code + message)
-        status = xml_wrap('_status', 'ERR')
-        output =  xml_wrap('resource', xmlerr + status)
-        return output
+        def route(self, rule, **options):
+            def decorator(f):
+                endpoint = options.pop('endpoint', None)
+                self.app.add_url_rule(rule, endpoint, f, **options)
+                return f
+            return decorator
 
-def xml_wrap(tag, text):
-    return '<' + tag + '>' + text + '</' + tag + '>'
 
-def start_eve():
-    global _proc
-    logger = logging.getLogger('Fmon')
-    logger.info('Starting Eve...')
-    _proc = Process(target=_start)
-    _proc.start()
+        @route(self, '/sensors')
+        def sensor_list():
+            return '\n'.join(self.al.all_sensors)
 
-def stop_eve():
-    _stop()
+        @route(self, '/sensorcount')
+        def sensor_count():
+            return str(len([x for x in self.al.all_sensors]))
 
-def _start():
-    global _proc
-    app.run(host='0.0.0.0', port=5000)
+        @route(self, '/lastreading/<sensor>')
+        def lastreading(sensor):
+            if sensor in self.al.ts_sensors:
+                return al.last_value(sensor)
+            else:
+                sl = ',\n'.join(self.al.ts_sensors)
+                msg = ('Unacceptible sensors. Please try:\n' + sl)
+                return msg
 
-def _stop():
-    _proc.terminate()
-    _proc.join()
+
+    def eve_start(self):
+        logger = logging.getLogger('Fmon')
+        logger.info('Starting Eve...')
+        self._start()
+
+        # def stop():
+        #     self._stop()
+
+    def _start(self):
+        self.app.run(host='0.0.0.0', port=5000)
+
+    def _stop():
+        _proc.terminate()
+        _proc.join()
 
 if __name__ == "__main__":
-    start_eve()
+    e = EveServer()
+    e.eve_start()
