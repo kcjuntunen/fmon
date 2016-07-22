adee56377654e7efd5e3abc14018bdbb9b28dd43
K. C. Juntunen <juntunen.kc@gmail.com>
2016-07-22 14:49:45 -0400

    generalize to config file

--- fmon/tests/query_test.py
+++ fmon/tests/query_test.py
@@ -1,8 +1,8 @@
 from unittest import TestCase
 # from unittest.mock import patch
 
-# from datetime import datetime
-# from pymongo.errors import BulkWriteError
+from datetime import datetime
+from pymongo.errors import BulkWriteError
 # import json
 
 from fmon import query
@@ -28,17 +28,19 @@ class TestCreate(TestCase):
         except BulkWriteError as bwe:
             print(bwe)
 
-        self.mc.timeseries_insert(testdata.light_not_transgressing)
-        self.mc.event_insert({'name': 'PIR', 'value': 0})
-        self.mc.event_insert({'name': 'Arduino reset', 'value': 0})
-
-        self.mc.event_insert({ 'Event': { 'name': 'PIR', 'value': 1 } })
-        self.mc.event_insert({ 'Event': { 'name': 'Shock', 'value': 1 } })
-        self.mc.event_insert({ 'Event': { 'name': 'Shock', 'value': 1 } })
-        self.mc.event_insert({ 'Event': { 'name': 'PIR', 'value': 0 } })
         # self.fmc = FMonConfiguration(self.mc)
         # self.alerts = Alerts(self.mc, self.fmc)
         self.al = query.ArduinoLog(db=self.mc._client, name=self.mc._db)
+        self.mc.timeseries_insert(testdata.light_not_transgressing)
+        asensor, bsensor, csensor = [x['sensor']
+                                     for x in testdata.config['sensors']
+                                     if x['type'] == 'event']
+        self.mc.event_insert({'name': csensor, 'value': 0})
+        self.mc.event_insert({'name': asensor, 'value': 0})
+        self.mc.event_insert({ 'Event': { 'name': asensor, 'value': 1 } })
+        self.mc.event_insert({ 'Event': { 'name': bsensor, 'value': 1 } })
+        self.mc.event_insert({ 'Event': { 'name': bsensor, 'value': 1 } })
+        self.mc.event_insert({ 'Event': { 'name': asensor, 'value': 0 } })
 
     def tearDown(self):
         # from time import sleep
@@ -80,8 +82,9 @@ class TestCreate(TestCase):
         self.assertEqual(aas, cas)
 
     def test_last_value(self):
-        lv = testdata.light_not_transgressing['tempF']
-        self.assertEqual(self.al.last_value('tempF'), lv)
+        asensor = testdata.config['sensors'][2]['sensor']
+        lv = testdata.light_not_transgressing[asensor]
+        self.assertEqual(self.al.last_value(asensor), lv)
 
     def test_last_values(self):
         lvs = testdata.light_not_transgressing
@@ -100,11 +103,12 @@ class TestCreate(TestCase):
         self.assertIsInstance(ev, Collection)
 
     def test_sample_count(self):
-        cnt = self.al.sample_count('tempF')
+        asensor = testdata.config['sensors'][2]['sensor']
+        cnt = self.al.sample_count(asensor)
         self.assertEqual(cnt, 1)
         self.mc.timeseries_insert(testdata.light_not_transgressing)
         self.mc.timeseries_insert(testdata.light_transgressing)
-        cnt = self.al.sample_count('tempF')
+        cnt = self.al.sample_count(asensor)
         self.assertEqual(cnt, 3)
 
     def test_latest_documents(self):
@@ -118,35 +122,40 @@ class TestCreate(TestCase):
         self.assertEqual(dd, testdata.light_not_transgressing)
 
     def test_hour_cursor(self):
+        asensor = testdata.config['sensors'][0]['sensor']
         self.mc.timeseries_insert(testdata.light_not_transgressing)
         self.mc.timeseries_insert(testdata.light_transgressing)
-        hc = self.al.hour_cursor('tempF')
+        hc = self.al.hour_cursor(asensor)
         vs = hc[0]['values']
         self.assertEqual(vs[len(vs) - 1],
                          testdata.light_transgressing['tempF'])
 
     def test_hour_list(self):
-        l = self.al.hour_list('hPa')
-        self.assertEqual(l[0], testdata.light_transgressing['hPa'])
+        asensor = testdata.config['sensors'][0]['sensor']
+        l = self.al.hour_list(asensor)
+        self.assertEqual(l[0], testdata.light_transgressing[asensor])
 
     def test_hour_events_cursor(self):
-        hec1 = self.al.hour_events_cursor('PIR')
-        hec2 = self.al.hour_events_cursor('Shock')
+        asensor = self.al.ev_sensors[0]
+        bsensor = self.al.ev_sensors[1]
+        hec1 = self.al.hour_events_cursor(asensor)
+        hec2 = self.al.hour_events_cursor(bsensor)
         l1 = [x for x in hec1]
         l2 = [x for x in hec2]
-        self.assertEqual(hec1.count(), 3)
-        self.assertEqual(len(l1), 3)
-        self.assertEqual(len(l2), 2)
-        self.assertEqual(hec2.count(), 2)
+        self.assertEqual(hec1.count(), 2)
+        self.assertEqual(len(l1), 2)
+        self.assertEqual(len(l2), 3)
+        self.assertEqual(hec2.count(), 3)
         print(l1)
-        self.assertEqual(l1[0]['name'], 'PIR')
-        self.assertEqual(l1[1]['name'], 'PIR')
+        self.assertEqual(l1[0]['name'], asensor)
+        self.assertEqual(l1[1]['name'], asensor)
         self.assertEqual(l1[1]['value'], 1)
-        self.assertEqual(l2[0]['name'], 'Shock')
+        self.assertEqual(l2[0]['name'], bsensor)
 
     def test_hour_event_list(self):
-        hel = self.al.hour_event_list('PIR')
-        self.assertEqual(hel[0]['name'], 'PIR')
+        ev = self.al.ev_sensors
+        hel = self.al.hour_event_list(ev[0])
+        self.assertEqual(hel[0]['name'], ev[0])
 
     def test_avg_hour(self):
         pass
@@ -164,19 +173,24 @@ class TestCreate(TestCase):
         pass
 
     def test_count_matches(self):
-        filter = { 'name': { '$in': ['tempF', 'hPa', 'light'] } }
-        self.assertEqual(self.al.count_matches(filter, 'timeseriesdata'), 3)
-        filter = { 'name': { '$in': ['Shock', 'PIR'] } }
-        self.assertEqual(self.al.count_matches(filter, 'eventdata'), 5)
+        filter = { 'name': { '$in': self.al.ts_sensors } }
+        self.assertEqual(self.al.count_matches(filter, 'timeseriesdata'),
+                         len(self.al.ts_sensors))
+        filter = { 'name': { '$in': self.al.ev_sensors } }
+        self.assertEqual(self.al.count_matches(filter, 'eventdata'),
+                         6)
 
     def test_count_values(self):
-        self.assertGreater(self.al.count_values('tempF'),
+        asensor = testdata.config['sensors'][0]['sensor']
+        bsensor = testdata.config['sensors'][2]['sensor']
+        self.assertGreater(self.al.count_values(asensor),
                            0)
-        self.assertGreater(self.al.count_values('hPa'),
+        self.assertGreater(self.al.count_values(bsensor),
                            0)
 
     def test_hour_stats(self):
-        hs = self.al.hour_stats('light')
+        asensor = testdata.config['sensors'][2]['sensor']
+        hs = self.al.hour_stats(asensor)
         for i in ['min', 'max', 'avg']:
             self.assertGreater(abs(hs[i]), 0)
 
@@ -184,7 +198,7 @@ class TestCreate(TestCase):
         self.assertEqual(len(self.al.list_events()), 6)
 
     def test_list_values(self):
-        lv = self.al.list_values('light')
-        self.assertEqual(lv[0]['name'], 'light')
+        asensor = testdata.config['sensors'][2]['sensor']
+        lv = self.al.list_values(asensor)
+        self.assertEqual(lv[0]['name'], asensor)
         self.assertEqual(lv[0]['ts'], query.current_hour())
-
